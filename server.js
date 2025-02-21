b71530ee358b70d94e0cbaa68832bdae921eb87e
austinechisutia <austineakhonya624@gmail.com>
2025-02-21 11:11:00 +0300

    local host

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use(express.static(initial_path));
 app.get('/', (req, res)=>{
     res.sendFile(path.join(initial_path, "index.html"))
 })
+app.get('/:id', (req, res)=>{
+    res.sendFile(path.join(initial_path, "about.html"))
+})
+app.use((req, res)=>{
+    res.json("404 page not found")
+})
 
 app.listen(300, ()=>{
     console.log('listening on port 300....');
