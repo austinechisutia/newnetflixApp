1092c97befd37001cfb7a0b5326f001ac2b0f7e5
austinechisutia <austineakhonya624@gmail.com>
2025-02-21 09:56:06 +0300

    getting started

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path')
+
+let initial_path = path.join(__dirname, "public");
+
+let app = express();
+app.use(express.static(initial_path));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(initial_path, "index.html"))
+})
+
+app.listen(300, ()=>{
+    console.log('listening on port 300....');
+    
+})
